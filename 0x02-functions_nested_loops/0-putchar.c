89a95ffe715579a5ed9afe41cbc5cab5df70e898
ibrahemesam <iimosa7777@gmail.com>
2023-09-12 19:19:25 +0300

    blabla bla

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-    char *string;
-    int i;
+	char *string;
+	int i;
 
-    string = "_putchar\n";
-    i = 0;
-    while (1)
-    {
-        char c;
+	string = "_putchar\n";
+	i = 0;
+	while (1)
+	{
+		char c;
 
-        c = string[i];
-        if (c == '\0')
-            break;
-        _putchar(c);
-        i++;
-    }
-    return 0;
+		c = string[i];
+		if (c == '\0')
+			break;
+		_putchar(c);
+		i++;
+	}
+	return (0);
 }
