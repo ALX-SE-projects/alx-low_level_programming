29b8c2a96d38727abc4826055f0ce126cc0c9c44
ibrahemesam <iimosa7777@gmail.com>
2023-09-12 20:02:56 +0300

    blabla bla

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ * print_sign - prints the sign of a number
+ * Return: -1 or 0 or 1
+ * @n: int to be checked
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
