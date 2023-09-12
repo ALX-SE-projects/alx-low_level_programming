a5996e1a7bf7a61e737ab7b2374e1cd8fe21b801
ibrahemesam <iimosa7777@gmail.com>
2023-09-12 20:34:06 +0300

    blabla bla

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,15 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_alphabet_x10();
+    int r;
+
+    r = print_last_digit(-2147483648);
+    putchar('\n');
     return (0);
 }
 
+int print_last_digit(int n)
+{
+	n = n % 10;
+    if (n < 0)
+	{
+		n = n * -1;
+	}
+	putchar(n + '0');
+	return (n);
+}
 
