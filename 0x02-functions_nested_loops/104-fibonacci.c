f6fb149a6873174707260fd0219878b87dd80504
ibrahemesam <iimosa7777@gmail.com>
2023-09-12 23:38:00 +0300

    blabla bla

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,30 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_DIGITS 1000
+
+void sumLargeNums(char n1[], char n2[], char r[]) {
+    int carr = 0;
+    int l1 = strlen(n1);
+    int l2 = strlen(n2);
+    int maxLen = (l1 > l2) ? l1 : l2;
+
+    for (int i = 0; i < maxLen; i++) {
+        int d1 = (i < l1) ? (n1[l1 - 1 - i] - '0') : 0;
+        int d2 = (i < l2) ? (n2[l2 - 1 - i] - '0') : 0;
+
+        int sum = d1 + d2 + carr;
+        r[maxLen - 1 - i] = (sum % 10) + '0';
+        carr = sum / 10;
+    }
+
+    if (carr != 0) {
+        memmove(r + 1, r, maxLen);
+        r[0] = carr + '0';
+        r[maxLen + 1] = '\0';
+    }
+}
 /**
  * putint - prints an int
  * @n: int to print
@@ -9,7 +35,7 @@ void putint(unsigned long int n)
 	{
 		putint(n / 10);
 	}
-	_putchar(n % 10 + '0');
+	putchar(n % 10 + '0');
 }
 /**
  * main - entry point
@@ -18,36 +44,30 @@ void putint(unsigned long int n)
 int main(void)
 {
 	int counter;
-	unsigned long int i;
-	unsigned long int j;
-	unsigned long int k;
-
-	i = 1;
-	j = 2;
-	putint(i);
-	_putchar(',');
-	_putchar(' ');
-	putint(j);
-	_putchar(',');
-	_putchar(' ');
+	char i[MAX_DIGITS] = "1";
+	char j[MAX_DIGITS] = "2";
+	printf("%s, ", i);
+	printf("%s, ", j);
 	counter = 2;
 	while (1)
 	{
-		k = i + j;
-		i = j;
-		j = k;
-		putint(k);
+		char k[MAX_DIGITS + 1];
+
+		sumLargeNums(i, j, k);
+		strcpy(i, j);
+		strcpy(j, k);
+		printf("%s", k);
 		counter++;
 		if (counter != 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 		else
 		{
 			break;
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
-void putint(long int num) {
-    if (num < 0) {
-        putchar('-');
-        num = -num;
+#define MAX_DIGITS 1000
+
+char[MAX_DIGITS + 1] sumLargeIntegers(char num1[], char num2[]) {
+    char result[MAX_DIGITS + 1];
+    int carry = 0;
+    int len1 = strlen(num1);
+    int len2 = strlen(num2);
+    int maxLen = (len1 > len2) ? len1 : len2;
+
+    for (int i = 0; i < maxLen; i++) {
+        int digit1 = (i < len1) ? (num1[len1 - 1 - i] - '0') : 0;
+        int digit2 = (i < len2) ? (num2[len2 - 1 - i] - '0') : 0;
+
+        int sum = digit1 + digit2 + carry;
+        result[maxLen - 1 - i] = (sum % 10) + '0';
+        carry = sum / 10;
     }
 
-    if (num / 10) {
-        putint(num / 10);
+    if (carry != 0) {
+        memmove(result + 1, result, maxLen);
+        result[0] = carry + '0';
+        result[maxLen + 1] = '\0';
     }
-
-    putchar(num % 10 + '0');
+    return result;
 }
 
 int main() {
-    long int number = 1234567890;
-    putint(number);
+    char num1[MAX_DIGITS] = "7540113804746346429";
+    char num2[MAX_DIGITS] = "12200160415121876738";
+
+
+
+    printf("Sum: %s\n", sumLargeIntegers(num1, num2, result));
+
     return 0;
 }
