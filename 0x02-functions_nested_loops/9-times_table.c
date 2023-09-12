38484cd2a442d1fc49f9dd3dcf8514972f9b04d4
ibrahemesam <iimosa7777@gmail.com>
2023-09-12 21:11:31 +0300

    blabla bla

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,9 +25,9 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i == 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j == 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			putint(i * j);
 			if (j == 9)
