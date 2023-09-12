1d5bafb2d43d162c79d91868917f1f06d4ba73a7
ibrahemesam <iimosa7777@gmail.com>
2023-09-12 23:42:45 +0300

    blabla bla

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,26 +4,44 @@
 
 #define MAX_DIGITS 1000
 
-void sumLargeNums(char n1[], char n2[], char r[]) {
-    int carr = 0;
-    int l1 = strlen(n1);
-    int l2 = strlen(n2);
-    int maxLen = (l1 > l2) ? l1 : l2;
+/**
+ * sumLargeNums - sum large numbers
+ * @n1: int_1
+ * @n2: int_2
+ * @r: result
+ */
+void sumLargeNums(char n1[], char n2[], char r[])
+{
+	int carr;
+	int l1;
+	int l2;
+	int maxLen;
+	int i;
 
-    for (int i = 0; i < maxLen; i++) {
-        int d1 = (i < l1) ? (n1[l1 - 1 - i] - '0') : 0;
-        int d2 = (i < l2) ? (n2[l2 - 1 - i] - '0') : 0;
+	carr = 0;
+	l1 = strlen(n1);
+	l2 = strlen(n2);
+	maxLen = (l1 > l2) ? l1 : l2;
 
-        int sum = d1 + d2 + carr;
-        r[maxLen - 1 - i] = (sum % 10) + '0';
-        carr = sum / 10;
-    }
+	for (i = 0; i < maxLen; i++)
+	{
+		int d1;
+		int d2;
+		int sum;
 
-    if (carr != 0) {
-        memmove(r + 1, r, maxLen);
-        r[0] = carr + '0';
-        r[maxLen + 1] = '\0';
-    }
+		d1 = (i < l1) ? (n1[l1 - 1 - i] - '0') : 0;
+		d2 = (i < l2) ? (n2[l2 - 1 - i] - '0') : 0;
+		sum = d1 + d2 + carr;
+		r[maxLen - 1 - i] = (sum % 10) + '0';
+		carr = sum / 10;
+	}
+
+	if (carr != 0)
+	{
+		memmove(r + 1, r, maxLen);
+		r[0] = carr + '0';
+		r[maxLen + 1] = '\0';
+	}
 }
 /**
  * putint - prints an int
@@ -46,6 +64,7 @@ int main(void)
 	int counter;
 	char i[MAX_DIGITS] = "1";
 	char j[MAX_DIGITS] = "2";
+
 	printf("%s, ", i);
 	printf("%s, ", j);
 	counter = 2;
