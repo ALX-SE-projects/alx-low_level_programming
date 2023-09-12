593bcc029f3039de24761798ec81594028051b1e
ibrahemesam <iimosa7777@gmail.com>
2023-09-12 21:30:21 +0300

    blabla bla

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * putint - prints an int
+ * @n: int to print
+ */
+void putint(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n *= -1;
+	}
+	if (n < 10)
+	{
+		putchar('0' + n);
+	}
+	else if (n < 100)
+	{
+		putchar('0' + (((n - (n % 10)) / 10) % 10));
+		putchar('0' + (n % 10));
+	}
+}
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by a \n
+ * @n: int to start from
+ */
+void print_to_98(int n)
+{
+	for (; n <= 98; n++)
+	{
+		putint(n);
+		if (n == 98)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+}
