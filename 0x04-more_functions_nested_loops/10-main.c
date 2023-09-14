51de6660ffc62d153a29ac8c4aa6ff2629dd8aa1
ibrahemesam <iimosa7777@gmail.com>
2023-09-14 18:51:29 +0300

    blabla bla

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_triangle(2);
+    printf("__________\n");
+    print_triangle(10);
+    printf("__________\n");
+    print_triangle(1);
+    printf("__________\n");
+    print_triangle(0);
+    printf("__________\n");
+    return (0);
+}
---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: is the size of the triangle
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, j, k, x;
+
+		for (i = 0; i < size; i++)
+		{
+			j++;
+			k = size - j;
+			for (x = 0; x < k; x++)
+			{
+				_putchar(' ');
+			}
+			for (x = 0; x < j; x++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
