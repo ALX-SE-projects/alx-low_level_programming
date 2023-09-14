64e437dedabd3e4ca2ff275a9122425de002a490
ibrahemesam <iimosa7777@gmail.com>
2023-09-14 18:52:47 +0300

    blabla bla

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -8,12 +8,8 @@
 int main(void)
 {
     print_triangle(2);
-    printf("__________\n");
     print_triangle(10);
-    printf("__________\n");
     print_triangle(1);
-    printf("__________\n");
     print_triangle(0);
-    printf("__________\n");
     return (0);
 }
