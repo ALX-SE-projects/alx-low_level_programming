1de2e0500056ac91ad96c3776f0fdb17c23cc45e
ibrahemesam <iimosa7777@gmail.com>
2023-09-14 17:10:23 +0300

    blabla bla

---
+++ 0x04-more_functions_nested_loops/0-isuper.c
@@ -0,0 +1,13 @@
+
+/**
+* _isupper - checks if char is uppercase
+* Return: 0 or 1
+* @c: char to be checked
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' || c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
