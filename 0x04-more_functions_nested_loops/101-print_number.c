ae21c36268f6267a9b7d844fae12262fe517d66e
ibrahemesam <iimosa7777@gmail.com>
2023-09-14 20:25:16 +0300

    blabla bla

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,32 +12,39 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	/* invert number */
-	n = (n * 10) + 1; /* append one to inverted num to retain front zeros*/
-	rev = 0;
-	digits = 0;
-	while (1)
+	if (n > 0)
 	{
-		digits++;
-		ones = n % 10;
-		n = (n - ones) / 10;
-		rev += ones;
-		if (n == 0)
+		/* invert number */
+		n = (n * 10) + 1; /* append one to inverted num to retain front zeros*/
+		rev = 0;
+		digits = 0;
+		while (1)
 		{
-			break;
+			digits++;
+			ones = n % 10;
+			n = (n - ones) / 10;
+			rev += ones;
+			if (n == 0)
+			{
+				break;
+			}
+			rev *= 10;
 		}
-		rev *= 10;
+		digits--;
+		/* print it as normal */
+		for (i = 0; i < digits; i++)
+		{
+			ones = rev % 10;
+			rev = (rev - ones) / 10;
+			_putchar('0' + ones);
+			if (rev == 0)
+			{
+				break;
+			}
+		}
 	}
-	digits--;
-	/* print it as normal */
-	for (i = 0; i < digits; i++)
+	else
 	{
-		ones = rev % 10;
-		rev = (rev - ones) / 10;
-		_putchar('0' + ones);
-		if (rev == 0)
-		{
-			break;
-		}
+		_putchar('0');
 	}
 }
