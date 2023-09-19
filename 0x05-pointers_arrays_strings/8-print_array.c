c7272a4740526e17023a3bd69aa9e93ee6750776
ibrahemesam <iimosa7777@gmail.com>
2023-09-19 06:07:05 +0300

    blabla bla

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_array - get length of a string.
+ * @a: pointer to string to calc its length
+ * @b: pointer to string to calc its length
+ */
+void print_array(int *a, int n)
+{
+	int xx;
+
+	for (xx = 0; xx < n; ++xx)
+	{
+		if (xx != (n - 1))
+			printf("%d, ", a[xx]);
+		else
+			printf("%d", a[xx]);
+	}
+	printf("\n");
+}
