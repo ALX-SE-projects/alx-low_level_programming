0618b40cf169c359a6bca94a7cdfebfbe07ae603
ibrahemesam <iimosa7777@gmail.com>
2023-09-19 06:22:08 +0300

    blabla bla

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int xx, ssss
+	int n;
+	int pass[100];
+
+	srand(time(NULL));
+	ssss = 0;
+	for (xx = 0; xx < 100; ++xx)
+	{
+		pass[xx] = rand() % 78;
+		ssss += (pass[xx] + '0');
+		putchar(pass[xx] + '0');
+		if ((2772 - ssss) - '0' < 78)
+		{
+			n = 2772 - ssss - '0';
+			ssss += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
