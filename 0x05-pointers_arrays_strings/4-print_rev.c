8b5ee6d4000a68307d703915359aa532bf0ce117
ibrahemesam <iimosa7777@gmail.com>
2023-09-19 05:42:19 +0300

    blabla bla

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _strlen - get length of a string.
+ * Return: int
+ * @s: pointer to string to calc its length
+ */
+int _strlen(char *s)
+{
+	int len, idx;
+
+	len = 0;
+	idx = 0;
+	while (1)
+	{
+		if (*(s + idx) != '\0')
+		{
+			len++;
+			idx++;
+		}
+		else
+		{
+			return (len);
+		}
+	}
+}
+/**
+ * print_rev -  prints a string, in reverse, followed by a \n.
+ * @s: pointer to string to print
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = _strlen(s);
+
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
