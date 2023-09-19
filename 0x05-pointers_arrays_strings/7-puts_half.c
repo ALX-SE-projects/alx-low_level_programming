d7838b5125e44f7cce976b222da5ba09d1902701
ibrahemesam <iimosa7777@gmail.com>
2023-09-19 06:04:28 +0300

    blabla bla

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * puts2 - get length of a string.
+ * @str: pointer to string to calc its length
+ */
+void puts_half(char *str)
+{
+	int oo, xx;
+
+	for (oo = 0; str[oo] != '\0'; ++oo)
+		;
+
+	if (oo % 2 == 0)
+	{
+		for (xx = oo / 2; str[xx] != '\0'; ++xx)
+			_putchar(str[xx]);
+	} else
+	{
+		for (xx = ((oo - 1) / 2) + 1; str[xx] != '\0'; ++xx)
+			_putchar(str[xx]);
+	}
+	_putchar('\n');
+}
