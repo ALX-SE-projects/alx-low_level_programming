affcbaf6571f94cd691999f420d49a7710bfc741
Ibrahem Essam <iimosa7777@gmail.com>
2023-09-21 21:48:35 +0000

    blabla

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * Return: encoded string
+ * @s: string to be encoded
+ */
+char *leet(char *s)
+{
+	int lc[] = {'a', 'e', 'o', 't', 'l'};
+	int uc[] = {'A', 'E', 'O', 'T', 'L'};
+	int lt[] = {'4', '3', '0', '7', '1'};
+	int i, j;
+
+	i = 0;
+	while (s[i])
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (lc[j] == s[i])
+			{
+				s[i] = lt[j];
+			}
+			if (uc[j] == s[i])
+			{
+				s[i] = lt[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
