e652aa7f44b36ce0926594c00311fb2e2f0d6fb9
ibrahemesam <iimosa7777@gmail.com>
2023-09-21 12:02:56 +0300

    blabla bla

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,19 +8,34 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int dst_len, src_len;
+	int dst_len, src_len, zero_rest;
 
 	dst_len = 0;
+	zero_rest = 0;
 	for (src_len = 0; src_len < n; src_len++)
 	{
 		if ((dest[dst_len] == 0) || (src[src_len] == 0))
 		{
+			if ((src[src_len] == 0) && (dest[dst_len] != 0))
+			{
+				zero_rest = 1;
+			}
 			break;
 		}
 		else
 		{
 			dest[dst_len] = src[src_len];
 			dst_len++;
+		}
+	}
+
+	while (zero_rest)
+	{
+		dest[dst_len] = 0;
+		dst_len++;
+		if (dest[dst_len] == 0)
+		{
+			break;
 		}
 	}
 
