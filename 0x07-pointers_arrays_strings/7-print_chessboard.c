ad1c948a3ee4c5e5d410114231e9333c628717b3
ibrahemesam <iimosa7777@gmail.com>
2023-09-26 03:41:20 +0300

    blabla bla

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,11 +9,9 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		j = 0;
-		while (a[i][j])
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
 		_putchar('\n');
 	}
