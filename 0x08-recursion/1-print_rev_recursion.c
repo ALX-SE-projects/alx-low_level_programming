131469224179f47d04f90bc26914ddf810dc9923
ibrahemesam <iimosa7777@gmail.com>
2023-09-28 15:44:14 +0300

    blabla bla

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+unsigned int get_str_len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return len + 1;
+}
+void _print_rev_recursion(char *s)
+{
+	int ln = get_str_len(s);
+}
