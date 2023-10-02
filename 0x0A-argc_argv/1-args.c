54b4677569724a8eb69f1afe91ab89a4f2f8ed51
ibrahemesam <iimosa7777@gmail.com>
2023-10-03 02:50:03 +0300

    blabla bla

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: args count
+ * @argv: list of args
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	_putchar('0' + argc);
+	_putchar('\n');
+	return (0);
+}
