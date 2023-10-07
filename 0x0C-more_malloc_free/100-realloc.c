4825d6a49ebddbf81ccdbccc74596db76d4e8638
ibrahemesam <iimosa7777@gmail.com>
2023-10-07 21:31:57 +0300

    blabla bla

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,5 +25,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	_ptr = (char *) malloc(new_size);
 	for (i = 0; i < old_size && i < new_size; i++)
 		_ptr[i] = ((char *) ptr)[i];
+	free(ptr);
 	return (_ptr);
 }
