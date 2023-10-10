efd3f548206df7ba703c7ef41733b2968b4b27bc
ibrahemesam <iimosa7777@gmail.com>
2023-10-11 01:56:35 +0300

    blabla bla

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,31 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "dog.h"
+/**
+ * _strlen - get length of a string.
+ * Return: int
+ * @s: pointer to string to calc its length
+ */
+int _strlen(char *s)
+{
+	int len, idx;
+
+	len = 0;
+	idx = 0;
+	while (1)
+	{
+		if (*(s + idx) != '\0')
+		{
+			len++;
+			idx++;
+		}
+		else
+		{
+			return (len);
+		}
+	}
+}
 /**
  * new_dog - creates a new dog
  * Return: dog_t
@@ -11,10 +37,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = (dog_t *) malloc(sizeof(dog_t));
 
-	if (!d)
+	if (d == NULL)
+	{
+		free(d);
 		return (NULL);
-	d->name = name;
+	}
+	d->name = malloc(sizeof(char) * _strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	d->age = age;
-	d->owner = owner;
+	d->owner = malloc(sizeof(char) * _strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->owner);
+		free(d);
+		return (NULL);
+	}
+	d->name = strcpy(d->name, name);
+	d->owner = strcpy(d->owner, owner);
 	return (d);
 }
