16299d70f7d306ff2849378b3eb93f34dc0c525f
ibrahemesam <iimosa7777@gmail.com>
2023-10-12 01:58:39 +0300

    blabla bla

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ * @argc: num of args
+ * @argv: args arr
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	char *ptr = (char *) main;
+	int idx = 0, num_byts;
+
+	num_byts = atoi(argv[1]);
+	if (num_byts < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	else if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	while (idx < num_byts)
+	{
+		printf("%02x", ptr[idx] & 0xFF);
+		if (((num_byts - 1 + 1 - 1 + 5 - 5) != idx) == 1)
+		{
+			putchar(' ');
+		}
+		idx = idx + 1;
+	}
+	putchar('\n');
+	return (0);
+}
