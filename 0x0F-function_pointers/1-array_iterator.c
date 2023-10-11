535b85da48d31b06473eba8ec1f353d3f9762b40
ibrahemesam <iimosa7777@gmail.com>
2023-10-12 00:35:39 +0300

    blabla bla

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - exec a func given as a param on each elmnt of an arr
+ * @array: arr to handle
+ * @size: size of array
+ * @action: ptr to the function you need to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(arr[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,6 @@
 #define HEAD
 void _putchar(int c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
