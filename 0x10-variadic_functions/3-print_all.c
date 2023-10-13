e75af3af93e946dd750df41556317d3d20219954
ibrahemesam <iimosa7777@gmail.com>
2023-10-14 00:39:52 +0300

    blabla bla

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list a;
-	unsigned int idx = 0;
+	unsigned int idx = 0, done = 0;
 	char c;
 	char *str;
 
@@ -21,22 +21,26 @@ void print_all(const char * const format, ...)
 	while (*(format + idx))
 	{
 		c = *(format + idx);
-		if ((c == 'c' || c == 'i' || c == 'f' || c == 's') && idx)
+		if ((c == 'c' || c == 'i' || c == 'f' || c == 's') && done)
 			printf("%s", ", ");
 		switch (c)
 		{
 			case 'c':
 				printf("%c", va_arg(a, int));
+				done++;
 				break;
 			case 'i':
 				printf("%d", va_arg(a, int));
+				done++;
 				break;
 			case 'f':
 				printf("%f", va_arg(a, double));
+				done++;
 				break;
 			case 's':
 				str = va_arg(a, char *);
 				printf("%s", str ? str : "(nil)");
+				done++;
 				break;
 		}
 		idx++;
