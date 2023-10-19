4b7433eff162ca606763bbd49faa4e9421d67cb6
ibrahemesam <iimosa7777@gmail.com>
2023-10-19 23:22:55 +0300

    blabla bla

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: head of that list
+ */
+void free_list(list_t *head)
+{
+	list_t *l = NULL, *tmp;
+	unsigned int last = 0;
+
+	if (head)
+	{
+		l = head;
+		while (l->next || last)
+		{
+			free(l->str);
+			if ((l->next) == NULL)
+				last = 0;
+			if (l->next)
+			{
+				tmp = l->next;
+				free(l);
+				l = tmp;
+			}
+		}
+	}
+}
