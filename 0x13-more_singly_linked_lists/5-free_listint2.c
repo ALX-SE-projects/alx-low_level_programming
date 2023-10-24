742462e5b3cf4d9d9db86cda02605efadc00cb62
ibrahemesam <iimosa7777@gmail.com>
2023-10-24 19:11:05 +0300

    blabla bla

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * and sets the head to NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *nxt;
+
+	while (*head)
+	{
+		nxt = (*head)->next;
+		free(*head);
+		head = &(nxt)
+	}
+}
