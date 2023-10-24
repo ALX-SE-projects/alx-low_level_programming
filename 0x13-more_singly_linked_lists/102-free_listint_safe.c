194115b78c3b1e24cac16a2601a583bf01a607dd
ibrahemesam <iimosa7777@gmail.com>
2023-10-25 00:50:00 +0300

    blabla bla

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: head of list
+ * Return:  size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp, *head = *h;
+	unsigned int count = 0;
+
+	while (head)
+	{
+		count++;
+		tmp = head->next;
+		if (head)
+		{
+			*h = NULL;
+			return (count - 1);
+		}
+		else
+		{
+			free(head);
+		}
+		head = tmp;
+	}
+	return (count);
+}
