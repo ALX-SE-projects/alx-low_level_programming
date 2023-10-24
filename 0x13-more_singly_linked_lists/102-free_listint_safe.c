94bd1fe5faaad585ca10cddcb84a40a0952fb1f7
ibrahemesam <iimosa7777@gmail.com>
2023-10-25 01:34:53 +0300

    blabla bla

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,28 +1,74 @@
 #include "lists.h"
 /**
+ * chek - check if node already printed
+ * @head: first node of list
+ * @ptr: node to check
+ * @until: idx to stop checking at
+ * Return: bool
+ */
+unsigned int chek(const listint_t *head, const void *ptr, int until)
+{
+	const listint_t *l = &(*head);
+	int i;
+
+	for (i = 0; i < until; i++)
+	{
+		if (ptr == l)
+			return (1);
+		l = l->next;
+	}
+	return (0);
+}
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: head of list
+ * Return: len of list
+ */
+size_t get_list_len(const listint_t *head)
+{
+	const listint_t *l = &(*head);
+	unsigned int count = 0;
+
+	if (head)
+	{
+		while (l)
+		{
+			count++;
+			if (chek(head, l, count - 1))
+			{
+				return (count - 1);
+			}
+			if (l->next)
+				l = l->next;
+			else
+				break;
+		}
+	}
+	return (count);
+}
+/**
  * free_listint_safe - frees a listint_t list.
  * @h: head of list
  * Return:  size of the list that was free’d
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tmp, *head = *h;
-	unsigned int count = 0;
+	const listint_t *ll;
+	listint_t *tmp = NULL, *l;
+	unsigned int len, i;
 
-	while (head)
+	if (h)
 	{
-		tmp = head->next;
-		if (head)
+		ll = *h;
+		len = get_list_len(ll);
+		l = *h;
+		for(i = 0; i < len; i++)
 		{
-			*h = NULL;
-			return (count);
+			tmp = l->next;
+			free(l);
+			l = tmp;
 		}
-		else
-		{
-			free(head);
-			count++;
-		}
-		head = tmp;
 	}
-	return (count);
+	*h = NULL;
+	return (len);
 }
