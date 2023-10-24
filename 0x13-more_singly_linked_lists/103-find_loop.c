47a9b2ff4b42a091c1a73b922d95604ba263d669
ibrahemesam <iimosa7777@gmail.com>
2023-10-25 01:46:18 +0300

    blabla bla

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,26 +1,5 @@
 #include "lists.h"
 /**
- * chok - check if node loops
- * @head: first node of list
- * @ptr: node to check
- * @until: idx to stop checking at
- * Return: bool
- */
-unsigned int chok(listint_t *head, const void *ptr, int until)
-{
-	const listint_t *l = &(*head);
-	int i;
-
-	for (i = 0; i < until; i++)
-	{
-		if (ptr == l)
-			return (1);
-		l = l->next;
-	}
-	return (0);
-}
-
-/**
  * find_listint_loop - finds the loop in a linked list.
  * @head: head of list
  *
