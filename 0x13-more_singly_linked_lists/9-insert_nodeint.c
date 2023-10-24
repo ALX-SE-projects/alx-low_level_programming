e9f4a954700273f8fc103668bb43bcd0da587475
ibrahemesam <iimosa7777@gmail.com>
2023-10-24 21:14:37 +0300

    blabla bla

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current = current->next;
 			idx--;
 		}
-		if (idx)
+		if (idx - 1 > 0)
 		{
 			free(new);
 			return (NULL);
