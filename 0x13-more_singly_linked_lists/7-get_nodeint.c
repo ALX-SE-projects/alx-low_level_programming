a73f3dcbc4150e0b04db245fe5099b4a851c4d11
ibrahemesam <iimosa7777@gmail.com>
2023-10-24 20:32:01 +0300

    blabla bla

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	if (!head)
 		return (NULL);
-	do {
+	while (index)
+	{
 		head = head->next;
 		if (!head)
 			break;
 		index--;
-	} while (index);
+	}
 	if (index)
 		return (NULL);
 	return (head);
-
 }
