a9743b8d7a8c7153dcd1bf8c9341471a64677738
ibrahemesam <iimosa7777@gmail.com>
2023-10-27 01:33:45 +0200

    blabla bla

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,38 @@
 #include "main.h"
 /**
+ * get_sizeof - find the len of @n
+ * @n: value to find its len
+ *
+ * Return: len
+*/
+int get_sizeof(unsigned long int n)
+{
+	int s = 0;
+
+	while (n > 0)
+	{
+		s++;
+		n >>= 1;
+	}
+	s--;
+	return (s);
+}
+/**
  * print_binary - prints the binary representation of a number.
  * @n: number
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int pb = 0, i;
+	unsigned int k = 1;
 
-	i = sizeof(n) * 8;
-	while (i)
+	if (k)
+		k <<= get_sizeof(n);
+	while (k > 0)
 	{
-		if (n & (1 << (i - 1)))
-		{
-			if (!pb)
-				pb = 1;
+		if (n & k)
 			_putchar('1');
-		}
 		else
-		{
-			if (pb)
-				_putchar('0');
-		}
-		i--;
+			_putchar('0');
+		k >>= 1;
 	}
-	if (!pb)
-		_putchar('0');
 }
