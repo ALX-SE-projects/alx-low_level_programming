28a4b09681d41f0ad838b4841207025769b04cd5
ibrahemesam <iimosa7777@gmail.com>
2023-10-27 02:58:44 +0200

    blabla bla

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,4 @@
 /**
- * get_sizeofff - find the len of @n
- * @n: value to find its len
- *
- * Return: len
-*/
-unsigned int get_sizeofff(unsigned long int n)
-{
-	unsigned int s = 0;
-
-	while (n > 0)
-	{
-		s++;
-		n >>= 1;
-	}
-	s--;
-	return (s);
-}
-/**
  * flip_bits - returns the number of bits you would
  *   need to flip to get from one number to another.
  * @n: num 1
@@ -26,20 +8,15 @@ unsigned int get_sizeofff(unsigned long int n)
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int r = 0, i, max;
-	unsigned int ln, lm;
+	unsigned int r = 0, i;
+	unsigned long int mask = 1;
 
-	ln = get_sizeofff(n);
-	lm = get_sizeofff(m);
-	if (ln > lm)
-		max = ln;
-	else
-		max = lm;
 	n ^= m;
-	for (i = 0; i <= max; i++)
+	for (i = 0; i < sizeof(n) * 8; i++)
 	{
-		if (n & (1 << i))
+		if (n & mask)
 			r++;
+		mask <<= 1;
 	}
 	return (r);
 }
