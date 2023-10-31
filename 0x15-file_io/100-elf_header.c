3a9d58d98b6f723b8600f94127b67b8868f482bc
ibrahemesam <iimosa7777@gmail.com>
2023-10-31 04:15:46 +0200

    blabla bla

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,123 +11,7 @@
 #define BALOO printf
 #define dBALOO dprintf
 #define FREE free
-/**
- * cls_fl - ...
- * @olf: ...
-*/
-void cls_fl(int olf)
-{
-	if (close(olf) == -1)
-	{
-		dBALOO(STDERR_FILENO,
-			"Error: Can't close fd %d\n", olf);
-		exit(98);
-	}
-}
-
-/**
- * pnt_mgc - ...
- * @i_edent: ...
-*/
-void pnt_mgc(unsigned char *i_edent)
-{
-	int idx;
-
-	idx = 0;
-	BALOO("  Magic    ");
-	while (idx < EI_NIDENT)
-	{
-		BALOO("%02x", i_edent[idx]);
-		if (idx != EI_NIDENT - 1)
-			BALOO(" ");
-		else
-			BALOO("\n");
-		idx++;
-	}
-}
-
-/**
- * chk_if_elf - checks ...
- * @i_edent: ...
-*/
-void chk_if_elf(unsigned char *i_edent)
-{
-	int idx;
-
-	idx = 0;
-	while (idx < 4)
-	{
-		if (i_edent[idx] != 127 &&
-		    i_edent[idx] != 'L' &&
-		    i_edent[idx] != 'E' &&
-		    i_edent[idx] != 'F')
-		{
-			dBALOO(STDERR_FILENO, "Error: Not an ELF file\n");
-			exit(98);
-		}
-		idx++;
-	}
-}
-
-/**
- * pnt_dta - ...
- * @i_edent: ...
-*/
-void pnt_dta(unsigned char *i_edent)
-{
-	BALOO("  Data:                              ");
-	switch (i_edent[EI_DATA])
-	{
-
-		case ELFDATA2LSB:
-			BALOO("2's complement, little endian\n");
-			break;
-		case ELFDATA2MSB:
-			BALOO("2's complement, big endian\n");
-			break;
-		case ELFDATANONE:
-			BALOO("none\n");
-			break;
-		default:
-			BALOO("<unknown: %x>\n", i_edent[EI_DATA]);
-	}
-}
-
-/**
- * pnt_cls - ...
- * @i_edent: ...
-*/
-void pnt_cls(unsigned char *i_edent)
-{
-	BALOO("  Class:                             ");
-	switch (i_edent[EI_CLASS])
-	{
-		case ELFCLASS32:
-			BALOO("ELF32\n");
-			break;
-		case ELFCLASS64:
-			BALOO("ELF64\n");
-			break;
-		case ELFCLASSNONE:
-			BALOO("none\n");
-			break;
-		default:
-			BALOO("<unknown: %x>\n", i_edent[EI_CLASS]);
-	}
-}
-
-/**
- * pnt_vsn - ...
- * @i_edent: ...
-*/
-void pnt_vsn(unsigned char *i_edent)
-{
-	BALOO("  Version:                           ");
-	if (i_edent[EI_VERSION] != EV_CURRENT)
-		BALOO("%i\n", i_edent[EI_VERSION]);
-	else
-		BALOO("%d (current)\n", i_edent[EI_VERSION]);
-}
+#include "main.h"
 
 /**
  * pnt_osb - ...
@@ -227,7 +111,7 @@ void pnt_entry(unsigned long int e_entry, unsigned char *i_edent)
 	BALOO("  Entry point address:               ");
 	if (i_edent[EI_DATA] == ELFDATA2MSB)
 	{
-		e_entry = (0xFF00FF00 &(e_entry << 8)) |
+		e_entry = (0xFF00FF00 & (e_entry << 8)) |
 			  (0xFF00FF & (e_entry >> 8));
 		e_entry = (e_entry >> 16) | (e_entry << 16);
 	}
@@ -239,7 +123,8 @@ void pnt_entry(unsigned long int e_entry, unsigned char *i_edent)
 }
 
 /**
- * main - ...
+ * main - ss
+ * Return: int
  * @argc: arg len
  * @argv: arg arr
 */
