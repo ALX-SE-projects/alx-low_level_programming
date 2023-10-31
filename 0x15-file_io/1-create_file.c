e08780647230a8e9082d736bce2625ddda74238d
ibrahemesam <iimosa7777@gmail.com>
2023-10-31 04:19:08 +0200

    blabla bla

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,16 +20,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IWUSR | S_IRUSR);
 	if (file == -1)
-		return (-1);
-	while (text_content)
+		return (file);
+	if (text_content)
 	{
 		while (text_content[wrds] != '\0')
 		{
 			wrds++;
 		}
-		if ((int) write(file, text_content, wrds) == -1)
+		if (write(file, text_content, wrds) == -1)
 			return (-1);
 	}
 	close(file);
