d9cbec3d9cd46ddf548bebcff543450bf113748b
ibrahemesam <iimosa7777@gmail.com>
2023-10-31 03:31:52 +0200

    blabla bla

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <elf.h>
+/**
+ * create_file - creates a file.
+ * @filename: filename
+ * @text_content: content in text
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, wrds = 0;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IWUSR | S_IRUSR);
+	if (file == -1)
+		return (file);
+	while (text_content)
+	{
+		while (text_content[wrds] != '\0')
+		{
+			wrds++;
+		}
+		if (write(file, text_content, wrds) == -1)
+			return (-1);
+	}
+	close(file);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef HD
 #define HD
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
