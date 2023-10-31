61c03a963aaa3b07bb17fdcb9dd53b2077b47068
ibrahemesam <iimosa7777@gmail.com>
2023-10-31 03:09:07 +0200

    blabla bla

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file
+ * @letters: length to read
+ *
+ * Return: length read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char c;
+	FILE *f;
+	unsigned int len = 0;
+
+	f = fopen(filename, "r");
+	while ((c = fgetc(f)) != EOF)
+	{
+		putchar(c);
+		len++;
+	}
+	fclose(file);
+	return (len);
+}
