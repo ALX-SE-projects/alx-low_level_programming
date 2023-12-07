6707c714caf68467da8b8b37f69fb71e0b07d9fd
ibrahemesam <iimosa7777@gmail.com>
2023-12-07 20:58:26 +0200

    blabla bla

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
  * dlistint_len -  returns the number of elements in a linked dlistint_t list
+ * @h: head of list
  * Return: size_t
  */
 size_t dlistint_len(const dlistint_t *h)
