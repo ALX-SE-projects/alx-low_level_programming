079bdbf3ce537959d4b8309acfa11b5a5738d1cc
ibrahemesam <iimosa7777@gmail.com>
2023-12-12 01:43:57 +0200

    blabla bla

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://github.com/ALX-SE-projects/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libwin.so > /tmp/libwin.so
+wget -P /tmp libwin.so https://github.com/ALX-SE-projects/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libwin.so
 export LD_PRELOAD=/tmp/libwin.so
