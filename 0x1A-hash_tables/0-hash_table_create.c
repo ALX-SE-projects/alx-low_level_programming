bd9486315fec33c1fdd27ced227755069491b11b
ibrahemesam <iimosa7777@gmail.com>
2023-12-22 22:23:23 +0200

    blabla bla

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(new->array))
 	{
-		free(new)
+		free(new);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
