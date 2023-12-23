f360bb46b59246eeb4bbf506dadbc69288819128
ibrahemesam <iimosa7777@gmail.com>
2023-12-23 04:37:57 +0200

    blabla bla

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -31,4 +31,20 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_node_t *make_hash_node(const char *key, const char *value);
+hash_node_t *make_hash_node(const char *key, const char *value);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_print(const shash_table_t *ht);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+void add_to_sorted_list(shash_table_t *table, shash_node_t *node);
+shash_node_t *make_shash_node(const char *key, const char *value);
+shash_table_t *shash_table_create(unsigned long int size);
 #endif
